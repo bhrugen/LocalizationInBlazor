65e3371ac2ec85e652a1dd1170f9d54211c2b9db
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-31 20:15:51 -0600

    Add project files.

---
+++ LocalizationInBlazor/Extensions/WebAssemblyHostExtension.cs
@@ -0,0 +1,34 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalizationInBlazor.Extensions
+{
+    public static class WebAssemblyHostExtension
+    {
+        public async static Task SetDefaultCulture(this WebAssemblyHost host)
+        {
+            var localStorage = host.Services.GetRequiredService<ILocalStorageService>();
+            var cultureFromLS = await localStorage.GetItemAsync<string>("culture");
+
+            CultureInfo culture;
+
+            if (cultureFromLS != null)
+            {
+                culture = new CultureInfo(cultureFromLS);
+            }
+            else
+            {
+                culture = new CultureInfo("en-US");
+            }
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+    }
+}
